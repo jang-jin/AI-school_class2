440aa99c93b55e682085d368cacb0524da398574
jang-jin <wkdwls98@gmail.com>
2020-07-09 12:11:37 +0900

    0709완료

--- 0708project3login+sqlite/login_json.py
+++ 0708project3login+sqlite/login_json.py
@@ -15,16 +15,16 @@ def make_data():
 # json 파일 읽어오기
 def load_data():
     try:
-        with open(path, 'r') as f:
+        with open(path, 'r', encoding='utf-8') as f:
             json_data = json.load(f)
         return json_data
     except:
         make_data()
-        load_data()
+        return load_data()
 
 # json 파일 저장하기
 def save_data(json_data):
-    with open(path, 'w') as make_file:
+    with open(path, 'w', encoding='utf-8') as make_file:
         json.dump(json_data, make_file, ensure_ascii=False, indent="\t")
 
 # 12자 이하 아이디 비밀번호 받기
@@ -36,16 +36,24 @@ def input_12_char_below(input_type):
 
     if len(input_data) > 12:
         print(f"{input_type}는 12자 이하로 입력해주세요!")
-        return ""
+        return None
+    elif len(input_data) <= 0:
+        print(f"{input_type}를 입력해주세요!")
+        return None
     else:
         return input_data.lower()
 
 # 중복 체크
 def duplicate_check(json_data, input_data):
-    for student in json_data['students']:
-        if student['userid'] == input_data:
-            print('이미 존재하는 id입니다. 다시 입력해주세요!')
-            return True
+    json_data
+    students = json_data['students']
+    if students:
+        for student in students:
+            if student['userid'] == input_data:
+                print('이미 존재하는 id입니다. 다시 입력해주세요!')
+                return True
+    else:
+        return False
 
 # 회원가입
 def join():
@@ -60,7 +68,7 @@ def join():
 
     while True:
         student['userid'] = input_12_char_below("아이디")
-        if not student['userid']:
+        if student['userid'] is None:
             continue
         if duplicate_check(json_data, student['userid']):
             continue
@@ -105,20 +113,80 @@ def login():
 
     while True:
         userid = input_12_char_below("아이디")
-        if not userid:
+        if userid is None:
             continue
         break
 
-    for student in json_data['students']:
+    students = json_data['students']
+    for student in students:
         if student['userid'] == userid:
             userpw = input_12_char_below("비밀번호")
             if student['password'] == userpw:
                 print(f"\n{student['name']}님 환영합니다!!\n")
                 return student['id']
             else:
-                print("잘못된 비밀번호입니다.")
+                print("잘못된 비밀번호입니다!")
+                break
+        elif student['id'] == len(students):
+            print("존재하지 않는 아이디입니다!")
 
+# 마이페이지
+def mypage(user_id):
+    json_data = load_data()
 
+    for student in json_data['students']:
+        if student['id'] == user_id:
+            print(f"아이디:{student['userid']}")
+            print(f"비밀번호:{student['password']}")
+            print(f"이름:{student['name']}")
+            print(f"나이:{student['age']}")
+    
+    for promise in json_data['promises']:
+        if promise['id'] == user_id:
+            print(f"앞으로 5개월 동안의 다짐:{promise['content']}")
+
+    for interest in json_data['interests']:
+        if interest['id'] == user_id:
+            print(f"관심분야:{interest['content']}\n")
+    
+# 다른 유저 정보 보기
+def anotherpage(user_id):
+    json_data = load_data()
+
+    user_ids = []
+    index = 1
+    for student in json_data['students']:
+        if student['id'] != user_id:
+            print(f"{index}. {student['name']}")
+            user_ids.append(student['id'])
+            index += 1
+
+    index = int(input("자세히볼 다른 유저를 선택하세요 : \n"))
+    
+# 서비스 메인
+def service_main(user_id):
+    while True:
+        menu = input("1. 마이페이지 보기 2. 다른 유저 보기 3. 로그아웃 : \n")
+        if menu == "1" :
+            mypage(user_id)
+        elif menu == "2" :
+        students_name = []
+        index = 1
+        for student in students:
+            if student['id'] != user_id:
+            print(f"{index}. {student['name']}")
+            students_name.append([student['name'],student['id']])
+            index += 1
+
+        index = int(input("자세히볼 다른 유저를 선택하세요 : \n"))
+        for student in students:
+            if student['id'] == students_name[index-1][1]:
+            print(f"아이디:{student['id']}\n이름:{student['name']}\n앞으로 5개월 동안의 다짐:{student['promise']}\n관심분야:{student['interests']}\n")
+        # elif menu == 3 :
+        # print("로그아웃되었습니다.")
+        # break
+        else :
+            print("잘못된 명령어입니다.")
 
 
 def main():
@@ -130,7 +198,8 @@ def main():
         if menu == "1":
             join()
         elif menu == "2":
-            login()
+            user_id = login()
+            service_main(user_id)
         elif menu == "3":
             print("프로그램을 종료합니다.")
             break
